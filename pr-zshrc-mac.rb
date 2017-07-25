acb60b6e2e3c727f2db234468e9db1fc19fcf82a
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-25 19:43:46 -0400

    initial commit

---
+++ pr-zshrc-mac.rb
@@ -0,0 +1,26 @@
+class Pr_zshrc < Formula
+    desc "My private zshrc configuration on my Mac OSX"
+    homepage "https://bitbucket.org/zijuexiansheng/bash_rc"
+    keg_only "This is only a script for source"
+    url "git@bitbucket.org:zijuexiansheng/bash_rc.git", :using => :git, :branch => "macosx"
+    version "0.1.0"
+    depends_on "cmake" => :build
+
+    def install
+        Dir.mkdir "build"
+        Dir.chdir "build" do
+            system "cmake", ".." "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_LOONLOCAL_DIR=${HOME}/loonlocal"
+            system "make", "install"
+        end
+    end
+
+    def caveats
+        <<-EOS.undent
+            This formula is keg-only, which means that brew will not link it.
+            In order to use it, you need to add the following to your .zshrc:
+                source #{opt_prefix}/bin/loonzsh.zsh
+
+            =================================================================
+        EOS
+    end
+end
