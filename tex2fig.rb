920296ebf0f8e42479f43d4b8bbe198e3e49369c
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-25 16:05:48 -0400

    fix bug

--- tex2fig.rb
+++ tex2fig.rb
@@ -1,9 +1,9 @@
 class Tex2fig < Formula
     desc "A tool to convert tex to independent figure in PDF format"
     homepage "https://github.com/zijuexiansheng/tex2fig"
-    url "https://github.com/zijuexiansheng/tex2fig.git", :using => :git, :revision => "3b63ad1ccf054ba38e9df78070e46e760d759d63"
+    url "https://github.com/zijuexiansheng/tex2fig.git", :using => :git, :revision => "753b5d903dfd38623c15d7d0bbdc668b64afff8e"
     head "https://github.com/zijuexiansheng/tex2fig.git", :using => :git
-    version "0.1.0"
+    version "0.1.1"
     depends_on "python" => :run
 
     def tex2fig_aux
@@ -11,7 +11,7 @@ class Tex2fig < Formula
     end
 
     def install
-        system "./install", "#{tex2fig_aux}"
+        system "./install.sh", "#{tex2fig_aux}"
         bin.install "bin/tex2fig"
         tex2fig_aux.install "script_dir/tex2fig.sh", "script_dir/tex2fig.tmpl"
     end
