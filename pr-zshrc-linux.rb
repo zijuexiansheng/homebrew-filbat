806d501d45575acf5ce141c4d7c186336f350f16
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-26 13:32:39 -0400

    update version

--- pr-zshrc-linux.rb
+++ pr-zshrc-linux.rb
@@ -3,7 +3,7 @@ class PrZshrcLinux < Formula
     homepage "https://bitbucket.org/zijuexiansheng/bash_rc"
     keg_only "This is only a script for source"
     url "git@bitbucket.org:zijuexiansheng/bash_rc.git", :using => :git, :branch => "linux"
-    version "0.1.0"
+    version "0.1.1"
     depends_on "cmake" => :build
 
     def install
