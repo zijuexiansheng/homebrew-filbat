e0cda6e9dddd6c147161f4d542b773ad6e2db4d3
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-26 13:26:42 -0400

    update version

--- pr-zshrc-mac.rb
+++ pr-zshrc-mac.rb
@@ -3,7 +3,7 @@ class PrZshrcMac < Formula
     homepage "https://bitbucket.org/zijuexiansheng/bash_rc"
     keg_only "This is only a script for source"
     url "git@bitbucket.org:zijuexiansheng/bash_rc.git", :using => :git, :branch => "macosx"
-    version "0.1.2"
+    version "0.1.3"
     depends_on "cmake" => :build
 
     def install
