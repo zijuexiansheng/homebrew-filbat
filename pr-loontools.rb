940adc6e38ebb463f1e04d9b5e328568487e687b
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-28 15:33:52 -0400

    add pr-loontools

---
+++ pr-loontools.rb
@@ -0,0 +1,21 @@
+class PrLoontools < Formula
+    desc "My private tools"
+    homepage "https://bitbucket.org/zijuexiansheng/loontools"
+    url "git@bitbucket.org:zijuexiansheng/loontools.git", :using => :git
+    version "0.1.0"
+    depends_on "cmake" => :build
+    depends_on "pr-loonlib" => :build
+
+    def install
+        magic_option = "None"
+        odie "Please set the magic_option first" if magic_option == "None"
+        Dir.mkdir "build"
+        Dir.chdir "build" do
+            system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=Release", "-DLOONLIB_ROOT_DIR=#{HOMEBREW_PREFIX}", "-D#{magic_option}_INSTALL=ON"
+            system "make", "install"
+        end
+        mv "loontools.zsh" "loontools"
+        inreplace "loontools", "=>libexec_dir<=", "#{libexec}"
+        bin.install "loontools"
+    end
+end
