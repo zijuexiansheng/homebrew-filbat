89488d8d6773cf9c7df8196a71793643bbeab76b
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-30 16:10:10 -0400

    update version

--- pr-loontools.rb
+++ pr-loontools.rb
@@ -2,7 +2,7 @@ class PrLoontools < Formula
     desc "My private tools"
     homepage "https://bitbucket.org/zijuexiansheng/loontools"
     url "git@bitbucket.org:zijuexiansheng/loontools.git", :using => :git
-    version "0.1.5"
+    version "0.1.6"
     depends_on "cmake" => :build
     depends_on "pr-loonlib" => :build
 
@@ -30,9 +30,8 @@ class PrLoontools < Formula
 
     def caveats
         <<-EOS.undent
-            * "loon_encrypt" and "loon_decrypt" are installed into directory #{libexec}/loon_crypt. Please add them to "loonmod" database
             * private directory has been created at #{loonlocaldir_private}
-            * "loontools", "ndssh", "ndscp" are installed into bin/ dir.
+            * "loontools", "ndssh", "ndscp", "load_password" are installed into bin/ dir.
             * Please double check your "magic_option"
             -------------------------------------------------------------------
         EOS
