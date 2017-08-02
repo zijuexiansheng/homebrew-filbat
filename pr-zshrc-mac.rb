f958948f54dc7eb8f7e56a3be0dc6901b079739e
zijuexiansheng <zijuexiansheng@gmail.com>
2017-08-02 13:42:20 -0400

    fix typo

--- pr-zshrc-mac.rb
+++ pr-zshrc-mac.rb
@@ -3,7 +3,7 @@ class PrZshrcMac < Formula
     homepage "https://bitbucket.org/zijuexiansheng/zshrc-mac"
     keg_only "This is only a script for source"
     url "git@bitbucket.org:zijuexiansheng/zshrc-mac.git", :using => :git
-    version "0.1.14"
+    version "0.1.15"
     depends_on "cmake" => :build
 
     def install
