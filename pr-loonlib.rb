b2f8fd357e2b3fb12bb059231084fc1014954db6
zijuexiansheng <zijuexiansheng@gmail.com>
2017-09-05 14:36:09 -0400

    add file encryptor/decryptor

--- pr-loonlib.rb
+++ pr-loonlib.rb
@@ -2,7 +2,7 @@ class PrLoonlib < Formula
     desc "My private library"
     homepage "https://bitbucket.org/zijuexiansheng/loonlib"
     url "git@bitbucket.org:zijuexiansheng/loonlib.git", :using => :git
-    version "0.1.10"
+    version "0.1.11"
     depends_on "cmake" => :build
 
     def install
