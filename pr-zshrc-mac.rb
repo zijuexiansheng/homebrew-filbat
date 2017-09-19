fdb1904e9c9eb476333a4948bec14b4fcc81f20e
zijuexiansheng <zijuexiansheng@gmail.com>
2017-09-19 13:57:14 -0400

    fix formula error

--- pr-zshrc-mac.rb
+++ pr-zshrc-mac.rb
@@ -3,14 +3,14 @@ class PrZshrcMac < Formula
     homepage "https://bitbucket.org/zijuexiansheng/zshrc-mac"
     keg_only "This is only a script for source"
     url "git@bitbucket.org:zijuexiansheng/zshrc-mac.git", :using => :git
-    version "0.1.27"
+    version "0.1.28"
     depends_on "zijuexiansheng/filbat/ccd" => :run
     depends_on "cmake" => :build
 
     def install
         Dir.mkdir "build"
         Dir.chdir "build" do
-            system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_LOONLOCAL_DIR=${HOME}/loonlocal", "-DCMAKE_BREWOPT_DIR=#{opt_prefix}"
+            system "cmake", "..", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_LOONLOCAL_DIR=${HOME}/loonlocal", "-DCMAKE_BREWOPT_DIR=#{HOMEBREW_PREFIX}/opt"
             system "make", "install"
         end
     end
