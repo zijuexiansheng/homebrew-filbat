f8338c23b0ddf78bf83fb9d432f5213a06611057
zijuexiansheng <zijuexiansheng@gmail.com>
2017-09-19 13:52:32 -0400

    add ccd; fix formula bug

--- pr-zshrc-mac.rb
+++ pr-zshrc-mac.rb
@@ -3,7 +3,8 @@ class PrZshrcMac < Formula
     homepage "https://bitbucket.org/zijuexiansheng/zshrc-mac"
     keg_only "This is only a script for source"
     url "git@bitbucket.org:zijuexiansheng/zshrc-mac.git", :using => :git
-    version "0.1.26"
+    version "0.1.27"
+    depends_on "zijuexiansheng/filbat/ccd" => :run
     depends_on "cmake" => :build
 
     def install
