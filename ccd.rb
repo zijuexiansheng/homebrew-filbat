f5b966b58c9aaa7764258c5be0fd467cd2947bd3
zijuexiansheng <zijuexiansheng@gmail.com>
2017-09-20 00:09:33 -0400

    fix regex matching bug

--- ccd.rb
+++ ccd.rb
@@ -3,7 +3,7 @@ class Ccd < Formula
     homepage "https://github.com/zijuexiansheng/convenient_cd"
     keg_only "This is only a script. Read the caveats above and ignore the ones below"
     url "https://github.com/zijuexiansheng/convenient_cd.git", :using => :git
-    version "0.1.3"
+    version "0.1.4"
     
     depends_on "python" => :run
 
