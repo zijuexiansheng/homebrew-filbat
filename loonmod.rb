ddb4b9522a4ba732085463f2b251a34f320691e8
zijuexiansheng <zijuexiansheng@gmail.com>
2017-12-28 20:53:38 +0800

    fix typo in the formula

--- loonmod.rb
+++ loonmod.rb
@@ -4,7 +4,7 @@ class Loonmod < Formula
     keg_only "This is only a script. Read the caveats above and ignore the ones below!!!"
     # url "https://github.com/zijuexiansheng/loonmod.git", :using => :git, :revision => "b9877d86290215d135831da2537e8c3619864033"
     # head "https://github.com/zijuexiansheng/loonmod.git", :using => :git
-    url "https://github.com/zijuexiansheng/loonmod.git", :using => :git,
+    url "https://github.com/zijuexiansheng/loonmod.git", :using => :git
     version "1.0.0"
     depends_on "python" => :run
     depends_on "zijuexiansheng/filbat/cmake@3.10" => :build
